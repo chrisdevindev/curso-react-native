c1fc7b94e1e80702ad65cf49b59357df2ba32c6a
Christian <alvescastro00cca13@gmail.com>
2020-10-31 15:00:08 -0300

    Tipos de parâmetros

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 const express = require("express") //importamos
 const app = express() //atribuímos as funções a variável
 
+app.use(express.json())
+
+
+//query params(listar, filtrar as mimnhas informações)
+//route params(identificar recursos -> atualizar e deletar)
+//request body(criar ou editar um recurso -> JSON)
 
 
 app.get('/projeto', (request, response) => {
+    const {title, dev} = request.query
+    console.log(title)
+    console.log(dev)
+
     return response.json([
        "Projeto 1",
        "Projeto 2"
@@ -12,6 +22,9 @@ app.get('/projeto', (request, response) => {
  
 
 app.post('/projeto', (request, response)=>{
+    const body = request.body
+    console.log(body)
+
     return response.json([
         "Projeto 1",
         "projeto 2",
@@ -20,6 +33,9 @@ app.post('/projeto', (request, response)=>{
 })
 
 app.put('/projeto/:id', (request, response) => {
+    const params = request.params
+    console.log(params) 
+
     return response.json([
         "Projeto 4",
         "projeto 2",
@@ -27,6 +43,8 @@ app.put('/projeto/:id', (request, response) => {
     ])
 })
 //http://localhost:3333/projeto/4
+
+
 
 app.delete('/projeto/:id', (request, response) => {
     return response.json([
