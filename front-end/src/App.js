3faf023b732bf0adc4abe11429e1ebe79c56a019
Christian <alvescastro00cca13@gmail.com>
2020-11-07 15:06:02 -0300

    Componentizaçaoo

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,8 +1,15 @@
+import React from 'react'
+import Header from './components/Header'
 
 function App() {
-  return (
-    <h1>Olá mundo</h1>
-  );
+  return(
+    <>
+        <Header/>
+        <Header/>
+        <Header/>
+        <Header/>
+    </>
+  )
 }
 
 export default App;
