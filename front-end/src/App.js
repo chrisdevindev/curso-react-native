2a6ade8414199658d13166d11a47bb76616af942
Christian <alvescastro00cca13@gmail.com>
2020-11-14 15:15:41 -0300

    Consumindo a API

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,9 +1,16 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import Header from './components/Header'
+import api from './services/api'
 
 function App() {
 
-  const [projects, setProjects] = useState(["Facebook", "Uber"])
+  const [projects, setProjects] = useState([])
+
+  useEffect(() => {
+    api.get('projeto').then(response => {
+      setProjects(response.data)
+    })
+  }, [])
 
   function handleAddProject(){
 
@@ -16,8 +23,8 @@ function App() {
         <Header title="React">
           <ul>
             {projects.map(project => (
-              <li key={project}>
-                {project}
+              <li key={project.id}>
+                {project.title}
               </li>
             ))}
           </ul>
@@ -32,3 +39,5 @@ function App() {
 }
 
 export default App;
+
+
